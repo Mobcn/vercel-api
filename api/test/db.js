dbe7c055f98d29111fc349542029961a929a85d1
Mobcn <mobingcn@gmail.com>
2023-09-19 14:24:31 +0800

    初始化

--- api/test/db.js
+++ api/test/db.js
@@ -5,6 +5,7 @@ import DB from '#db';
  * @param {import('#handler').VercelResponse} response
  */
 export default function handler(request, response) {
+    response.setHeader('Content-Type', 'text/html;charset=UTF-8');
     DB.connect()
         .then(() => response.status(200).end('连接成功！'))
         .catch((error) => response.status(500).end(`连接失败: ${error.message}`))
