9f916894bd5a1531ea1a4a13ce105b3822912037
Mobcn <mobingcn@gmail.com>
2023-09-27 19:35:55 +0800

    初始化

---
+++ api/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {import("#handler").VercelRequest} request 请求对象
+ * @param {import("#handler").VercelResponse} response 响应对象
+ */
+export default function handler(request, response) {
+    const { pathname } = new URL(request.url, `http://${request.headers.host}`);
+    console.log(`#controller${pathname}.js`);
+    import(`#controller${pathname}.js`)
+        .then(({ default: handler }) => handler(request, response))
+        .catch((error) => {
+            if (error.code === 'ERR_MODULE_NOT_FOUND') {
+                response.status(404).end();
+            } else {
+                response.status(500).end(error.message);
+            }
+        });
+}
