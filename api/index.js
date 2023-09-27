8182ad592d9d6681441def23cdb45a8e71896194
Mobcn <mobingcn@gmail.com>
2023-09-27 19:45:39 +0800

    初始化

--- api/index.js
+++ api/index.js
@@ -4,8 +4,7 @@
  */
 export default function handler(request, response) {
     const { pathname } = new URL(request.url, `http://${request.headers.host}`);
-    console.log(`#controller${pathname}.js`);
-    import(`#controller${pathname}.js`)
+    import(`../controller${pathname}.js`)
         .then(({ default: handler }) => handler(request, response))
         // .catch((error) => {
         //     if (error.code === 'ERR_MODULE_NOT_FOUND') {
