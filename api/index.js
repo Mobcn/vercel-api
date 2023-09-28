f5305877baf2ce4143fb229244c4e16db1e28794
Mobcn <mobingcn@gmail.com>
2023-09-28 12:10:50 +0800

    初始化

--- api/index.js
+++ api/index.js
@@ -1,42 +1,23 @@
-// import blogArticleGet from '#controller/blog/article/get.js';
-// import blogArticleList from '#controller/blog/article/list.js';
-// import blogArticleRemove from '#controller/blog/article/remove.js';
-// import blogArticleSave from '#controller/blog/article/save.js';
-// import blogArticleUpdate from '#controller/blog/article/update.js';
-// import blogCategoryAll from '#controller/blog/category/all.js';
-// import blogCategoryRemove from '#controller/blog/category/remove.js';
-// import blogCategorySave from '#controller/blog/category/save.js';
-// import blogSettingList from '#controller/blog/setting/list.js';
-// import blogSettingRemove from '#controller/blog/setting/remove.js';
-// import blogSettingSave from '#controller/blog/setting/save.js';
-// import blogTagAll from '#controller/blog/tag/all.js';
-// import blogTagRemove from '#controller/blog/tag/remove.js';
-// import blogTagSave from '#controller/blog/tag/save.js';
-// import blogUserAll from '#controller/blog/user/all.js';
-// import blogUserDestroy from '#controller/blog/user/destroy.js';
-// import blogUserLogin from '#controller/blog/user/login.js';
-// import blogUserRegister from '#controller/blog/user/register.js';
-
-// const router = {
-//     '/blog/article/get': blogArticleGet,
-//     '/blog/article/list': blogArticleList,
-//     '/blog/article/remove': blogArticleRemove,
-//     '/blog/article/save': blogArticleSave,
-//     '/blog/article/update': blogArticleUpdate,
-//     '/blog/category/all': blogCategoryAll,
-//     '/blog/category/remove': blogCategoryRemove,
-//     '/blog/category/save': blogCategorySave,
-//     '/blog/setting/list': blogSettingList,
-//     '/blog/setting/remove': blogSettingRemove,
-//     '/blog/setting/save': blogSettingSave,
-//     '/blog/tag/all': blogTagAll,
-//     '/blog/tag/remove': blogTagRemove,
-//     '/blog/tag/save': blogTagSave,
-//     '/blog/blog/user/all': blogUserAll,
-//     '/blog/blog/user/destroy': blogUserDestroy,
-//     '/blog/blog/user/login': blogUserLogin,
-//     '/blog/blog/user/register': blogUserRegister
-// };
+const router = {
+    '/blog/article/get': import('#controller/blog/article/get.js'),
+    '/blog/article/list': import('#controller/blog/article/list.js'),
+    '/blog/article/remove': import('#controller/blog/article/remove.js'),
+    '/blog/article/save': import('#controller/blog/article/save.js'),
+    '/blog/article/update': import('#controller/blog/article/update.js'),
+    '/blog/category/all': import('#controller/blog/category/all.js'),
+    '/blog/category/remove': import('#controller/blog/category/remove.js'),
+    '/blog/category/save': import('#controller/blog/category/save.js'),
+    '/blog/setting/list': import('#controller/blog/setting/list.js'),
+    '/blog/setting/remove': import('#controller/blog/setting/remove.js'),
+    '/blog/setting/save': import('#controller/blog/setting/save.js'),
+    '/blog/tag/all': import('#controller/blog/tag/all.js'),
+    '/blog/tag/remove': import('#controller/blog/tag/remove.js'),
+    '/blog/tag/save': import('#controller/blog/tag/save.js'),
+    '/blog/blog/user/all': import('#controller/blog/user/all.js'),
+    '/blog/blog/user/destroy': import('#controller/blog/user/destroy.js'),
+    '/blog/blog/user/login': import('#controller/blog/user/login.js'),
+    '/blog/blog/user/register': import('#controller/blog/user/register.js')
+};
 
 /**
  * @param {import("#handler").VercelRequest} request 请求对象
@@ -44,15 +25,11 @@
  */
 export default function handler(request, response) {
     const { pathname } = new URL(request.url, `http://${request.headers.host}`);
-    // if (Object.keys(router).includes(pathname)) {
-    //     try {
-    //         router[pathname](request, response);
-    //     } catch (error) {
-    //         response.status(500).end(error.message);
-    //     }
-    // } else {
-    //     response.status(404).end();
-    // }
-    import('#controller/blog/tag/all.js')
-        .then(({ default: handler }) => handler(request, response))
+    if (Object.keys(router).includes(pathname)) {
+        Promise.resolve(router[pathname])
+            .then(({ default: handler }) => handler(request, response))
+            .catch((error) => response.status(500).end(error.message));
+    } else {
+        response.status(404).end();
+    }
 }
