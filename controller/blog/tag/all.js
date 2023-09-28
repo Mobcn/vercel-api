0c11386254c68ed34605092596ffcd99c2382b73
Mobcn <mobingcn@gmail.com>
2023-09-28 12:17:22 +0800

    初始化

--- controller/blog/tag/all.js
+++ controller/blog/tag/all.js
@@ -4,4 +4,4 @@ import { tagService } from '#service/blog/TagService.js';
 /**
  * 获取标签列表
  */
-export default VHandler.buildGET(async () => await tagService.listAll());
+export default VHandler.buildGET(async () => import.meta);
